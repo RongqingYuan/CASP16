853681e9da11b000d6756b6713e7fedd1ab3829a
Rongqing Yuan <yuanrongqingyrq@gmail.com>
2024-08-27 02:20:04 -0400

    finish paired t-test code

--- monomer/step8_paired_t-test.py
+++ monomer/step8_paired_t-test.py
@@ -12,8 +12,11 @@ from matplotlib import pyplot as plt
 from factor_analyzer import FactorAnalyzer, calculate_kmo, calculate_bartlett_sphericity
 from copy import deepcopy, copy
 import time
+import scipy.stats as stats
 
 csv_path = "./monomer_data/raw/"  # read raw csv files
+csv_path = "./monomer_data/raw_data/EU/"
+
 csv_list = [txt for txt in os.listdir(csv_path) if txt.endswith(".csv")]
 
 out_path = "./by_target/"
@@ -70,23 +73,96 @@ measures = data_whole.columns
 print(measures)
 
 
-# measure_of_interest = "GDT_TS"
-# measure_of_interest_1 = "RMS_CA"
-
-# get the column for the measure of interests and measure of interests 1
-# data_whole = data_whole[[measure_of_interest, measure_of_interest_1]]
-use_domain = True
-if use_domain:
+# TODO
+# let's keep it as it is for now
+# this part will convert format like T1270TS274_2-D2 to T1270-D2TS274_2
+use_EU = True
+if use_EU:
     data_whole.index = data_whole.index.str.replace(
         r'(\w+)(TS\d+_\d+)-(\w+)', r'\1-\3\2')
+data_whole.index = data_whole.index.str.extract(
+    r'(?P<Level1>.+TS)(?P<Level2>\d+)_(?P<Level3>\d+)')
+print(data_whole.head())
+data_whole.index = pd.MultiIndex.from_tuples(
+    data_whole.index, names=['target', 'group', 'submission_id'])
+print(data_whole.head())
+measure_of_interest = "GDT_TS"
+data_whole = data_whole[measure_of_interest]
+data_whole = pd.DataFrame(data_whole)
+data_whole = data_whole.stack().unstack('group')
+# add submission_id to the index
+# print the keys of data_whole
+data_whole.index = [f'{target}_{submission_id}' for target,
+                    submission_id, measure in data_whole.index]
+
+# get a new column called submission_id and target
+data_whole['target'] = data_whole.index.str.split('_').str[0]
+data_whole['submission_id'] = data_whole.index.str.split('_').str[1]
+# remove any submission_id = 6
+data_whole = data_whole[data_whole['submission_id'] != '6']
+#  for each target, fill the missing values with the mean of the target
+#  if the whole group is missing, fill with 0
+data_whole_by_target = data_whole.groupby('target')
+
+
+def fill_missing(group):
+    # 计算组内每列的均值（不包括 target 和 submission_id 列）
+    means = group.drop(columns=['submission_id']).mean()
+
+    # 用均值填补缺失值
+    filled_group = group.fillna(means)
+
+    # 如果整组数据都是缺失值，则用0填补
+    filled_group = filled_group.fillna(0)
+
+    return filled_group
+
+
+data_whole_by_target = data_whole_by_target.apply(
+    fill_missing).reset_index(drop=True)
+
+
+# TODO
+# these two lines are also working
+# grouped_means = data_whole.groupby('target').transform('mean')
+# data_whole_by_target = data_whole.fillna(grouped_means)
+
+print(data_whole_by_target.head())
+data_whole_by_target.to_csv('./tmp/test.csv')
+wanted_group = ["052", "022", "456", "051",
+                "319", "287", "208", "028", "019", "294", "465", "110", "345", "139"]
+
+# now we have a good data shape we can run the paired t-test
+
+points = {}
+for group_1 in wanted_group:
+    for group_2 in wanted_group:
+        if group_1 == group_2:
+            continue
+        data_1 = data_whole_by_target[group_1]
+        data_1 = pd.DataFrame(data_1)
+        data_2 = data_whole_by_target[group_2]
+        data_2 = pd.DataFrame(data_2)
+        # run the paired t-test
+        t_stat, p_val = stats.ttest_rel(data_1, data_2)
+        print(
+            f"Group {group_1} vs Group {group_2}: t_stat={t_stat}, p_val={p_val}")
+        if group_1 not in points:
+            points[group_1] = 0
+        if t_stat > 0 and p_val/2 < 0.01:
+            points[group_1] += 1
+
+# sort the points
+points = dict(sorted(points.items(), key=lambda x: x[1], reverse=True))
+print(points)
+sys.exit(0)
 
 data_whole['prefix'] = data_whole.index.str.split('_').str[0]
 data_whole['group'] = data_whole.index.str.split('_').str[1]
-print(data_whole)
 print(data_whole.shape)
+print(data_whole.head())
 
-sys.exit(0)
-data_whole_grouped = data_whole.groupby('prefix').max()
+data_whole_grouped = data_whole.groupby('prefix').min()
 print(data_whole_grouped.shape)
 print(data_whole_grouped.head())
 print(measures)
