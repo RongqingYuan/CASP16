32cbedb7e30b106503d58ad043ac4f84eb08ea86
Rongqing Yuan <yuanrongqingyrq@gmail.com>
2024-09-05 14:15:10 -0400

    finish sum best code

--- monomer/step11_sum_z-score_best.py
+++ monomer/step11_sum_z-score_best.py
@@ -11,60 +11,30 @@ csv_list = [txt for txt in os.listdir(
 
 # read all data and concatenate them into one big dataframe
 data = pd.DataFrame()
+feature = "GDT_TS"
 for csv_file in csv_list:
     print("Processing {}".format(csv_file))
     data_tmp = pd.read_csv(csv_path + csv_file, index_col=0)
-    data_tmp = pd.DataFrame(data_tmp["GDT_TS"])
+    data_tmp = pd.DataFrame(data_tmp[feature])
     print(data_tmp.shape)
-    if data_tmp.shape[1] == 35:
-        print("something wrong with {}".format(csv_file))
-        sys.exit(0)
     data_tmp.index = data_tmp.index.str.extract(
         r'(T\w+)TS(\w+)_(\w+)-(D\w+)').apply(lambda x: (f"{x[0]}-{x[3]}", f"TS{x[1]}", x[2]), axis=1)
-
-    # print(data_tmp.shape)
-    # print(data_tmp.head())
     data_tmp.index = pd.MultiIndex.from_tuples(
         data_tmp.index, names=['target', 'group', 'submission_id'])
-    # save the target name
-    # target_name = data_tmp.index.get_level_values("target")[0]
-    # grouped = score_df.groupby(["target", "group"])
+    # # get all data with submission_id == 6
+    # data_tmp = data_tmp.loc[(slice(None), slice(None), "6"), :]
+    # drop all data with submission_id == 6
+    # data_tmp = data_tmp.loc[(slice(None), slice(None), [
+    #                          "1", "2", "3", "4", "5"]), :]
+    data_tmp = data_tmp.loc[(slice(None), slice(None),
+                             "1"), :]
     grouped = data_tmp.groupby(["group", "target"])
-    # print(grouped.head())
-    # print(type(grouped))
-
-    feature = "GDT_TS"
     grouped = pd.DataFrame(grouped[feature].max())
-    # print(grouped.head())
-    # grouped = pd.DataFrame(grouped["low_resolution_score"].max())
-    # grouped = pd.DataFrame(grouped["chemical_score"].max())
-    # sum the scores for each group
-    print(grouped.head())
-    result = grouped.groupby("group")
-    print(type(result))
-    # convert the groupby object to a dataframe
-    result = result.apply(lambda x: x)
-    print(result.head())
-    print(type(result))
-    result.index = result.index.droplevel(1)
-    # normalize the scores using z-score
-    # print(result.head())
-    # result = result.apply(lambda x: (x - x.min()) / (x.max() - x.min()))
-    result = result.apply(lambda x: (x - x.mean()) / x.std())
-    # print(result.head())
-    # result.to_csv("./tmp/" + "grouped_results.csv")s
-    # breakpoint()
-    # sort grouped by value
-    # grouped = grouped.sort_values(by="low_resolution_score", ascending=False)
-    # grouped = grouped.sort_values(by="chemical_score", ascending=False)
-    # print(grouped)
-    # print(result.head())
-    # change the column name to the target name
-    result = result.rename(columns={"GDT_TS": csv_file.split(".")[0]})
     grouped.index = grouped.index.droplevel(1)
-    grouped = grouped.apply(lambda x: (x - x.mean()) / x.std())
-    grouped = grouped.rename(columns={"GDT_TS": csv_file.split(".")[0]})
-    print(grouped.head())
+    # grouped = grouped.apply(lambda x: (x - x.mean()) / x.std())
+
+    # change the column name to the target name
+    grouped = grouped.rename(columns={feature: csv_file.split(".")[0]})
     data = pd.concat([data, grouped], axis=1)
 
 
