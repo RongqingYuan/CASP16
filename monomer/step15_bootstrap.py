4f882dd73899db2eca0199b23463c8a445552c82
Rongqing Yuan <yuanrongqingyrq@gmail.com>
2024-09-07 02:00:43 -0400

    bootstrap code; can generate good figures now

--- monomer/step15_bootstrap.py
+++ monomer/step15_bootstrap.py
@@ -1,18 +1,119 @@
+import seaborn as sns
+import scipy.stats as stats
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
 import sys
 
 
-from scipy import stats
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
 score_path = "./by_target/"
-measure = "GDT_HA"
 measure = "RMSD[L]"
 measure = "RMS_CA"
+measure = "GDT_HA"
 measure = "GDT_TS"
-score_file = "groups_by_targets_for-{}-EU.csv".format(measure)
 score_file = "groups_by_targets_for-raw-{}-EU.csv".format(measure)
+score_file = "groups_by_targets_for-{}-EU.csv".format(measure)
+
+
+hard_group = [
+    "T1207-D1",
+    "T1210-D1",
+    "T1210-D2",
+    "T1220s1-D1",
+    "T1220s1-D2",
+    "T1226-D1",
+    "T1228v1-D3",
+    "T1228v1-D4",
+    "T1228v2-D3",
+    "T1228v2-D4",
+    "T1239v1-D4",
+    "T1239v2-D4",
+    "T1271s1-D1",
+    "T1271s3-D1",
+    "T1271s8-D1",
+]
+
+medium_group = [
+    "T1210-D3",
+    "T1212-D1",
+    "T1218-D1",
+    "T1218-D2",
+    "T1227s1-D1",
+    "T1228v1-D1",
+    "T1228v2-D1",
+    "T1230s1-D1",
+    "T1237-D1",
+    "T1239v1-D1",
+    "T1239v1-D3",
+    "T1239v2-D1",
+    "T1239v2-D3",
+    "T1243-D1",
+    "T1244s1-D1",
+    "T1244s2-D1",
+    "T1245s2-D1",
+    "T1249v1-D1",
+    "T1257-D3",
+    "T1266-D1",
+    "T1270-D1",
+    "T1270-D2",
+    "T1267s1-D1",
+    "T1267s1-D2",
+    "T1267s2-D1",
+    "T1269-D1",
+    "T1269-D2",
+    "T1269-D3",
+    "T1271s2-D1",
+    "T1271s4-D1",
+    "T1271s5-D1",
+    "T1271s5-D2",
+    "T1271s7-D1",
+    "T1271s8-D2",
+    "T1272s2-D1",
+    "T1272s6-D1",
+    "T1272s8-D1",
+    "T1272s9-D1",
+    "T1279-D2",
+    "T1284-D1",
+    "T1295-D1",
+    "T1295-D3",
+    "T1298-D1",
+    "T1298-D2",
+]
+
+easy_group = [
+    "T1201-D1",
+    "T1201-D2",
+    "T1206-D1",
+    "T1208s1-D1",
+    "T1208s2-D1",
+    "T1218-D3",
+    "T1228v1-D2",
+    "T1228v2-D2",
+    "T1231-D1",
+    "T1234-D1",
+    "T1235-D1",
+    "T1239v1-D2",
+    "T1239v2-D2",
+    "T1240-D1",
+    "T1240-D2",
+    "T1245s1-D1",
+    "T1246-D1",
+    "T1257-D1",
+    "T1257-D2",
+    "T1259-D1",
+    "T1271s6-D1",
+    "T1274-D1",
+    "T1276-D1",
+    "T1278-D1",
+    "T1279-D1",
+    "T1280-D1",
+    "T1292-D1",
+    "T1294v1-D1",
+    "T1294v2-D1",
+    "T1295-D2",
+    # "T1214",
+]
 
 
 score_matrix = pd.read_csv(score_path + score_file, index_col=0)
@@ -22,24 +123,47 @@ print(score_matrix.shape)
 print(score_matrix.head())
 print(T1_data.shape)
 print(T1_data.head())
+T1_data_tmp = T1_data.copy()
+# impute na with 0
+T1_data_tmp.fillna(0, inplace=True)
+sum = T1_data_tmp.sum(axis=1)
+T1_data_tmp['sum'] = sum
+T1_data_tmp = T1_data_tmp.sort_values(by='sum', ascending=False)
+
+mode = "all"
+mode = "easy"
+mode = "hard"
+mode = "medium"
+if mode == "all":
+    pass
+elif mode == "easy":
+    T1_data = T1_data[easy_group]
+elif mode == "medium":
+    T1_data = T1_data[medium_group]
+elif mode == "hard":
+    T1_data = T1_data[hard_group]
+
+
+# breakpoint()
+
 T1_data = T1_data.T
 print(T1_data.shape)
 print(T1_data.head())
 # drop columns with more than 80% values missing
 T1_data = T1_data.loc[:, T1_data.isnull().mean() < 0.8]
 # fill na with 0
-T1_data.fillna(50, inplace=True)
+if "raw" in score_file:
+    T1_data.fillna(50, inplace=True)
+else:
+    T1_data.fillna(-2, inplace=True)
 print(T1_data.shape)
 print(T1_data.head())
 # fill na with 0
 groups = T1_data.columns
-# run paired t-test for group pairs
-total_count = 0
-significant_count = 0
+
+
 points = {}
-# create a 2d array to see if one wins over the other
 length = len(groups)
-win_matrix = [[0 for i in range(length)] for j in range(length)]
 
 for i in range(length):
     for j in range(length):
@@ -49,50 +173,85 @@ for i in range(length):
             continue
         group_1_data = T1_data[group_1]
         group_2_data = T1_data[group_2]
-        # run paired t-test
-        import scipy.stats as stats
         t_stat, p_value = stats.ttest_rel(group_1_data, group_2_data)
-        print("Group 1: {}, Group 2: {}, t-stat: {}, p-value: {}".format(
-            group_1, group_2, t_stat, p_value))
-        total_count += 1
-        if p_value/2 < 0.05:
-            significant_count += 1
+        # print("Group 1: {}, Group 2: {}, t-stat: {}, p-value: {}".format(group_1, group_2, t_stat, p_value))
         if t_stat > 0 and p_value/2 < 0.05:
             if group_1 not in points:
                 points[group_1] = 0
             points[group_1] += 1
-            win_matrix[i][j] = 1
-print("Total count: {}, Significant count: {}".format(
-    total_count, significant_count))
-# sort the points ascending = False
 points = dict(sorted(points.items(), key=lambda x: x[1], reverse=True))
 print(points)
-# plot the win matrix, if 1 use grey, if 0 use white
-plt.imshow(win_matrix, cmap='Greys', interpolation='nearest')
-plt.show()
-plt.savefig("./tmp/win_matrix.png")
 
+# use the above t-test code to get a initial ranking of the groups.
+# then generate new groups list using the ranking
+# then do bootstrapping
+groups = list(points.keys())
+# breakpoint()
+
+points = {}
+length = len(groups)
+win_matrix = [[0 for i in range(length)] for j in range(length)]
+bootstrap_rounds = 100
+
+for r in range(bootstrap_rounds):
+    T1_data_bootstrap = T1_data.sample(frac=1, replace=True)
+    for i in range(length):
+        for j in range(length):
+            group_1 = groups[i]
+            group_2 = groups[j]
+            if group_1 == group_2:
+                continue
+            group_1_data = T1_data_bootstrap[group_1]
+            group_2_data = T1_data_bootstrap[group_2]
+            # run paired t-test
+            t_stat, p_value = stats.ttest_rel(group_1_data, group_2_data)
+            # print("Group 1: {}, Group 2: {}, t-stat: {}, p-value: {}".format(group_1, group_2, t_stat, p_value))
+            if t_stat > 0 and p_value/2 < 0.05:
+                if group_1 not in points:
+                    points[group_1] = 0
+                points[group_1] += 1
+                win_matrix[i][j] += 1
+    print("Round: {}".format(r))
+
+# breakpoint()
+points = dict(sorted(points.items(), key=lambda x: x[1], reverse=True))
+print(points)
+# plot the win matrix as heatmap using seaborn
+# only the color should be plotted, do not show the numbers
+plt.figure(figsize=(30, 25))
+sns.heatmap(win_matrix, annot=False, cmap='Greys', cbar=True, square=True,)
+# set the axis labels to be the group names
+plt.xticks(np.arange(length), groups, rotation=45, ha='right', fontsize=10)
+plt.yticks(np.arange(length), groups, rotation=0, fontsize=10)
+plt.show()
+plt.savefig(
+    "./bootstrap/win_matrix_bootstrap_{}_n={}.png".format(mode, bootstrap_rounds), dpi=300)
+
+sys.exit(0)
 
 # group 1 is the first group in the ranked points
 group_1 = list(points.keys())[0]
 # group 2 is the second group in the ranked points
 group_2 = list(points.keys())[1]
 # now do bootstrapping
-rounds = 5000
+rounds = 10000
 group_1_wins = []
 group_2_wins = []
 no_difference = []
 for i in range(rounds):
     sampled_data = T1_data.sample(frac=1, replace=True)
+    # print(sampled_data.shape)
     group_1_data = sampled_data[group_1]
     group_2_data = sampled_data[group_2]
     t_stat, p_value = stats.ttest_rel(group_1_data, group_2_data)
-    if t_stat > 0 and p_value/2 < 0.05:
+    if t_stat > 0 and p_value/2 < 0.01:
         group_1_wins.append(1)
-    elif t_stat < 0 and p_value/2 < 0.05:
+    elif t_stat < 0 and p_value/2 < 0.01:
         group_2_wins.append(1)
     else:
         no_difference.append(1)
+print("Score: {}".format(measure))
+print("Group 1: {}, Group 2: {}".format(group_1, group_2))
 print("Group 1 wins: {}, Group 2 wins: {}".format(
     len(group_1_wins), len(group_2_wins)))
 print("No difference: {}".format(len(no_difference)))
