ca978ed1caace963660a38b63919428513ce8d7f
Rongqing Yuan <yuanrongqingyrq@gmail.com>
2024-09-11 00:15:57 -0400

    compare different phases

--- monomer/step17_compare_phase_bar.py
+++ monomer/step17_compare_phase_bar.py
@@ -27,9 +27,9 @@ T0_data = T0_data.T
 T1_data = T1_data.T
 T2_data = T2_data.T
 
-stages = ["T1", "T2"]
 stages = ["T0", "T1"]
-
+stages = ["T1", "T2"]
+top_n = 20
 if stages == ["T0", "T1"]:
     # 提取xxx部分
     T0_data.index = T0_data.index.str.extract(r'T0([-\w]+)')[0]
@@ -40,49 +40,55 @@ if stages == ["T0", "T1"]:
     df0_aligned = T0_data.loc[common_index]
     df1_aligned = T1_data.loc[common_index]
     # 计算差值
-    df_diff = df1_aligned - df0_aligned
-    # breakpoint()
+    df0_aligned = df0_aligned.T
+    df1_aligned = df1_aligned.T
 
-    # 结果
-    print(df1_aligned.shape)
-    print(df0_aligned.shape)
-    print(df_diff.shape)
+    targets = df0_aligned.columns
+    diff_valid_numbers = []
+    diff_valid_dict = {}
+    for target in targets:
+        df0_aligned_target = df0_aligned[target]
+        df1_aligned_target = df1_aligned[target]
+        # take the intersection of the two dataframes where both are not null
+        # 布尔索引，选择两个列都不为 NaN 的行
+        valid_rows = df0_aligned_target.notna() & df1_aligned_target.notna()
+        # 取出交集部分
+        df0_aligned_valid = df0_aligned_target[valid_rows]
+        df1_aligned_valid = df1_aligned_target[valid_rows]
+        # 计算差值
+        # df_diff_valid = df1_aligned_valid - df0_aligned_valid
+        # get top_n means for df0_aligned_valid and df1_aligned_valid
+        df0_aligned_valid = list(df0_aligned_valid)
+        df1_aligned_valid = list(df1_aligned_valid)
+        diff_valid_number = len(df0_aligned_valid)
+        df0_aligned_valid = df0_aligned_valid[:top_n]
+        df1_aligned_valid = df1_aligned_valid[:top_n]
+        df0_aligned_valid_mean = sum(
+            df0_aligned_valid) / len(df0_aligned_valid)
+        df1_aligned_valid_mean = sum(
+            df1_aligned_valid) / len(df1_aligned_valid)
+        diff_valid = df1_aligned_valid_mean - df0_aligned_valid_mean
+        diff_valid_dict[target] = diff_valid
+        diff_valid_numbers.append(diff_valid_number)
+        # breakpoint()
+    # sort the diff_valid_dict by value
+    diff_valid_dict = dict(
+        sorted(diff_valid_dict.items(), key=lambda item: item[1], reverse=True))
+    print(diff_valid_dict)
+    print(sorted(diff_valid_numbers, reverse=True))
 
-    df_diff.fillna(0, inplace=True)
-    print(df_diff.shape)
-
-    # 对行进行 Ward 聚类
-    row_linkage = sch.linkage(df_diff, method='ward')
-
-    # 对列进行 Ward 聚类
-    col_linkage = sch.linkage(df_diff.T, method='ward')
-
-    # # 绘制热图和树状图
-    # sns.clustermap(score_matrix, row_linkage=row_linkage,
-    #                col_linkage=col_linkage, cmap="viridis", figsize=(40, 30), dendrogram_ratio=(0.15, 0.15))
-    # plt.savefig("./by_target_png/clustermap-{}.png".format(measure), dpi=300)
-
-    # 生成clustermap
-    g = sns.clustermap(df_diff,
-                       row_linkage=row_linkage,
-                       col_linkage=col_linkage,
-                       cmap="coolwarm",
-                       figsize=(40, 15),
-                       dendrogram_ratio=(0.15, 0.15),
-                       vmin=-80,  # 设定最小值为 -n
-                       vmax=80)
-
-    # 调整x轴和y轴的字体大小
-    plt.setp(g.ax_heatmap.get_xticklabels(), fontsize=12)  # 调整x轴字体大小
-    plt.setp(g.ax_heatmap.get_yticklabels(), fontsize=12)  # 调整y轴字体大小
-    # rotate x-axis labels
-    plt.setp(g.ax_heatmap.get_xticklabels(), rotation=45)
-    # align to the right
-    plt.setp(g.ax_heatmap.get_xticklabels(), ha='right')
-    # set the title
-    g.fig.suptitle("Clustermap-{}".format(measure), fontsize=30)
-    plt.savefig(
-        "./stages_png/clustermap_{}-{}-{}.png".format(stages[0], stages[1], measure), dpi=300)
+    # plot the diff_valid_dict
+    plt.figure(figsize=(20, 10))
+    plt.bar(diff_valid_dict.keys(), diff_valid_dict.values())
+    plt.xticks(rotation=45, ha='right', fontsize=10)
+    plt.ylabel("{} of {} - {}".format(measure,
+               stages[1], stages[0]), fontsize=12)
+    # draw a horizontal line at y=0
+    plt.axhline(0, color='black', linestyle='-')
+    plt.title("Mean difference between {} of {} and {} for top {}".format(
+        measure, stages[1], stages[0], top_n), fontsize=14)
+    plt.savefig("./stages_png/" +
+                "T1_T0_diffs_{}_top_{}.png".format(measure, top_n), dpi=300)
 
 elif stages == ["T1", "T2"]:
     # 提取xxx部分
@@ -94,46 +100,51 @@ elif stages == ["T1", "T2"]:
     df0_aligned = T1_data.loc[common_index]
     df1_aligned = T2_data.loc[common_index]
     # 计算差值
-    df_diff = df1_aligned - df0_aligned
-    # breakpoint()
+    df0_aligned = df0_aligned.T
+    df1_aligned = df1_aligned.T
 
-    # 结果
-    print(df1_aligned.shape)
-    print(df0_aligned.shape)
-    print(df_diff.shape)
-
-    df_diff.fillna(0, inplace=True)
-    print(df_diff.shape)
-
-    # 对行进行 Ward 聚类
-    row_linkage = sch.linkage(df_diff, method='ward')
-
-    # 对列进行 Ward 聚类
-    col_linkage = sch.linkage(df_diff.T, method='ward')
-
-    # # 绘制热图和树状图
-    # sns.clustermap(score_matrix, row_linkage=row_linkage,
-    #                col_linkage=col_linkage, cmap="viridis", figsize=(40, 30), dendrogram_ratio=(0.15, 0.15))
-    # plt.savefig("./by_target_png/clustermap-{}.png".format(measure), dpi=300)
-
-    # 生成clustermap
-    g = sns.clustermap(df_diff,
-                       row_linkage=row_linkage,
-                       col_linkage=col_linkage,
-                       cmap="coolwarm",
-                       figsize=(40, 30),
-                       dendrogram_ratio=(0.15, 0.15),
-                       vmin=-80,  # 设定最小值为 -n
-                       vmax=80)
-
-    # 调整x轴和y轴的字体大小
-    plt.setp(g.ax_heatmap.get_xticklabels(), fontsize=12)  # 调整x轴字体大小
-    plt.setp(g.ax_heatmap.get_yticklabels(), fontsize=12)  # 调整y轴字体大小
-    # rotate x-axis labels
-    plt.setp(g.ax_heatmap.get_xticklabels(), rotation=45)
-    # align to the right
-    plt.setp(g.ax_heatmap.get_xticklabels(), ha='right')
-    # set the title
-    g.fig.suptitle("Clustermap-{}".format(measure), fontsize=30)
-    plt.savefig(
-        "./stages_png/clustermap_{}-{}-{}.png".format(stages[0], stages[1], measure), dpi=300)
+    targets = df0_aligned.columns
+    diff_valid_numbers = []
+    diff_valid_dict = {}
+    for target in targets:
+        df0_aligned_target = df0_aligned[target]
+        df1_aligned_target = df1_aligned[target]
+        # take the intersection of the two dataframes where both are not null
+        # 布尔索引，选择两个列都不为 NaN 的行
+        valid_rows = df0_aligned_target.notna() & df1_aligned_target.notna()
+        # 取出交集部分
+        df0_aligned_valid = df0_aligned_target[valid_rows]
+        df1_aligned_valid = df1_aligned_target[valid_rows]
+        # 计算差值
+        # df_diff_valid = df1_aligned_valid - df0_aligned_valid
+        # get top_n means for df0_aligned_valid and df1_aligned_valid
+        df0_aligned_valid = list(df0_aligned_valid)
+        df1_aligned_valid = list(df1_aligned_valid)
+        diff_valid_number = len(df0_aligned_valid)
+        df0_aligned_valid = df0_aligned_valid[:top_n]
+        df1_aligned_valid = df1_aligned_valid[:top_n]
+        df0_aligned_valid_mean = sum(
+            df0_aligned_valid) / len(df0_aligned_valid)
+        df1_aligned_valid_mean = sum(
+            df1_aligned_valid) / len(df1_aligned_valid)
+        diff_valid = df1_aligned_valid_mean - df0_aligned_valid_mean
+        diff_valid_dict[target] = diff_valid
+        diff_valid_numbers.append(diff_valid_number)
+        # breakpoint()
+    # sort the diff_valid_dict by value
+    diff_valid_dict = dict(
+        sorted(diff_valid_dict.items(), key=lambda item: item[1], reverse=True))
+    print(diff_valid_dict)
+    print(sorted(diff_valid_numbers, reverse=True))
+    # plot the diff_valid_dict
+    plt.figure(figsize=(20, 10))
+    plt.bar(diff_valid_dict.keys(), diff_valid_dict.values())
+    plt.ylabel("{} of {} - {}".format(measure,
+                                      stages[1], stages[0]), fontsize=12)
+    # draw a horizontal line at y=0
+    plt.axhline(0, color='black', linestyle='-')
+    plt.xticks(rotation=45, ha='right', fontsize=10)
+    plt.title("Mean difference between {} of {} and {} for top {}".format(
+        measure, stages[1], stages[0], top_n), fontsize=14)
+    plt.savefig("./stages_png/" +
+                "T2_T1_diffs_{}_top_{}.png".format(measure, top_n), dpi=300)
