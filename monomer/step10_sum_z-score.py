6a9cc02e144b1f2a20ef7c0e9e01724e1272decd
Rongqing Yuan <yuanrongqingyrq@gmail.com>
2024-09-24 01:36:40 -0400

    code for processing raw txt files

--- monomer/step10_sum_z-score.py
+++ monomer/step10_sum_z-score.py
@@ -1,6 +1,107 @@
+import argparse
 import pandas as pd
 import numpy as np
 import os
+
+
+def get_group_by_target(csv_list, csv_path, sum_path,
+                        feature, model, mode, impute_value=-2):
+    inverse_columns = ["RMS_CA", "RMS_ALL", "err",
+                       "RMSD[L]", "MolPrb_Score", "FlexE", "MP_clash", "MP_rotout", "MP_ramout"]
+    if not os.path.exists(out_path + f"impute={impute_value}/"):
+        os.makedirs(out_path + f"impute={impute_value}/")
+    if not os.path.exists(out_path + "raw/"):
+        os.makedirs(out_path + "raw/")
+    if not os.path.exists(sum_path + f"impute={impute_value}/"):
+        os.makedirs(sum_path + f"impute={impute_value}/")
+
+    data = pd.DataFrame()
+    data_raw = pd.DataFrame()
+    for csv_file in csv_list:
+        print(f"Processing {csv_file}")
+        data_tmp = pd.read_csv(csv_path + csv_file, index_col=0)
+        data_tmp = pd.DataFrame(data_tmp[feature])
+        # if there is "-" in the value, replace it with 0
+        data_tmp = data_tmp.replace("-", float(0))
+        if feature in inverse_columns:
+            data_tmp[feature] = -data_tmp[feature]
+        data_tmp.index = data_tmp.index.str.extract(
+            r'(T\w+)TS(\w+)_(\w+)-(D\w+)').apply(lambda x: (f"{x[0]}-{x[3]}", f"TS{x[1]}", x[2]), axis=1)
+        data_tmp.index = pd.MultiIndex.from_tuples(
+            data_tmp.index, names=['target', 'group', 'submission_id'])
+        # # get all data with submission_id == 6
+        # data_tmp = data_tmp.loc[(slice(None), slice(None), "6"), :]
+        # drop all data with submission_id == 6
+        if model == "best":
+            data_tmp = data_tmp.loc[(slice(None), slice(None), [
+                "1", "2", "3", "4", "5"]), :]
+        elif model == "first":
+            data_tmp = data_tmp.loc[(slice(None), slice(None),
+                                    "1"), :]
+        grouped = data_tmp.groupby(["group"])
+        grouped = pd.DataFrame(grouped[feature].max())
+        # grouped.index = grouped.index.droplevel(1)
+        grouped[feature] = grouped[feature].astype(float)
+        grouped = grouped.sort_values(by=feature, ascending=False)
+        initial_z = (grouped - grouped.mean()) / grouped.std()
+        new_z_score = pd.DataFrame(
+            index=grouped.index, columns=grouped.columns)
+        filtered_data = grouped[feature][initial_z[feature] >= -2]
+        new_mean = filtered_data.mean(skipna=True)
+        new_std = filtered_data.std(skipna=True)
+        new_z_score[feature] = (grouped[feature] - new_mean) / new_std
+        new_z_score = new_z_score.fillna(impute_value)
+        new_z_score = new_z_score.where(
+            new_z_score > impute_value, impute_value)
+        new_z_score = new_z_score.rename(
+            columns={feature: csv_file.split(".")[0]})
+        data = pd.concat([data, new_z_score], axis=1)
+
+        grouped = grouped.rename(columns={feature: csv_file.split(".")[0]})
+        data_raw = pd.concat([data_raw, grouped], axis=1)
+    # impute data again with impute_value, because some group may not submit all targets
+    data = data.fillna(impute_value)
+
+    # nan_values = data.isnull().sum().sum()
+    # if nan_values > 0:
+    #     breakpoint()
+    #     raise ValueError(
+    #         "There are still NaN values in the data. Please check the data again.")
+
+    # sort columns by alphabetical order
+    data = data.reindex(sorted(data.columns), axis=1)
+    data_columns = data.columns
+    data.to_csv(
+        out_path + f"impute={impute_value}/" + f"group_by_target-{feature}-{model}-{mode}.csv")
+    target_count = {}
+    for EU in data_columns:
+        target = EU.split("-")[0]
+        if target not in target_count:
+            target_count[target] = 0
+        target_count[target] += 1
+    # use the inverse of the target_count as the weight
+    target_weight = {key: 1/value for key, value in target_count.items()}
+    # assign EU_weight based on the target_weight
+    EU_weight = {EU: target_weight[EU.split("-")[0]]
+                 for EU in data_columns}
+
+    data["sum"] = data.sum(axis=1)
+    data = data.sort_values(by="sum", ascending=False)
+    data.to_csv(
+        sum_path + f"impute={impute_value}/" + f"sum_unweighted_{feature}-{model}-{mode}.csv")
+
+    data.drop(columns=["sum"], inplace=True)
+    data = data * pd.Series(EU_weight)
+    data["sum"] = data.sum(axis=1)
+    data = data.sort_values(by="sum", ascending=False)
+    data.to_csv(
+        sum_path + f"impute={impute_value}/" + f"sum_{feature}-{model}-{mode}.csv")
+
+    data_raw = data_raw.reindex(sorted(data_raw.columns), axis=1)
+    data_raw_csv = f"groups_by_targets_for-raw-{feature}-{model}-{mode}.csv"
+    data_raw.to_csv
+    data_raw.to_csv(
+        out_path + "raw/" + data_raw_csv)
 
 
 hard_group = [
@@ -103,39 +204,42 @@ easy_group = [
 ]
 
 
-csv_path = "./monomer_data_aug_30/processed/EU/"
-csv_path = "./monomer_data_Sep_10/processed/EU/"
-csv_path = "./monomer_data_Sep_10/raw_data/EU/"
+# csv_path = "./monomer_data_aug_30/processed/EU/"
+# csv_path = "./monomer_data_Sep_10/processed/EU/"
+# csv_path = "./monomer_data_Sep_10/raw_data/EU/"
+csv_path = "./monomer_data_Sep_15_EU/raw_data/"
 csv_list = [txt for txt in os.listdir(
     csv_path) if txt.endswith(".csv") and txt.startswith("T1")]
+out_path = "./group_by_target_EU/"
+sum_path = "./sum_EU/"
+if not os.path.exists(out_path):
+    os.makedirs(out_path)
+if not os.path.exists(sum_path):
+    os.makedirs(sum_path)
 
-model = "first"
+# model = "first"
 model = "best"
 
-mode = "easy"
-mode = "medium"
-mode = "hard"
+# mode = "easy"
+# mode = "medium"
+# mode = "hard"
 mode = "all"
+impute_value = -2
+
 
 if mode == "hard":
     csv_list = [csv for csv in csv_list if csv.split(
         ".")[0] in hard_group]
-
 elif mode == "medium":
     csv_list = [csv for csv in csv_list if csv.split(
         ".")[0] in medium_group]
-
 elif mode == "easy":
     csv_list = [csv for csv in csv_list if csv.split(
         ".")[0] in easy_group]
-
 elif mode == "all":
     pass
 
 
-# print(csv_list.__len__())
-# breakpoint()
-# read all data and concatenate them into one big dataframe
 feature = "GDT_TS"
 features = ['GDT_TS',
             'GDT_HA', 'GDC_SC', 'GDC_ALL', 'RMS_CA', 'RMS_ALL', 'AL0_P',
@@ -144,79 +248,8 @@ features = ['GDT_TS',
             'CAD_AA', 'RPF', 'TMscore', 'FlexE', 'QSE', 'CAD_SS', 'MP_clash',
             'MP_rotout', 'MP_ramout', 'MP_ramfv', 'reLLG_lddt', 'reLLG_const']
 
-inverse_columns = ["RMS_CA", "RMS_ALL", "err",
-                   "RMSD[L]", "MolPrb_Score", "FlexE", "MP_clash", "MP_rotout", "MP_ramout"]
-
-
-def get_group_by_target(csv_list, csv_path, feature, model, mode):
-    data = pd.DataFrame()
-    for csv_file in csv_list:
-        print("Processing {}".format(csv_file))
-        data_tmp = pd.read_csv(csv_path + csv_file, index_col=0)
-        data_tmp = pd.DataFrame(data_tmp[feature])
-        # if there is "-" in the value, replace it with 0
-        data_tmp = data_tmp.replace("-", float(0))
-        # print(data_tmp.shape)
-        if feature in inverse_columns:
-            data_tmp[feature] = -data_tmp[feature]
-        data_tmp.index = data_tmp.index.str.extract(
-            r'(T\w+)TS(\w+)_(\w+)-(D\w+)').apply(lambda x: (f"{x[0]}-{x[3]}", f"TS{x[1]}", x[2]), axis=1)
-        data_tmp.index = pd.MultiIndex.from_tuples(
-            data_tmp.index, names=['target', 'group', 'submission_id'])
-        # # get all data with submission_id == 6
-        # data_tmp = data_tmp.loc[(slice(None), slice(None), "6"), :]
-        # drop all data with submission_id == 6
-        if model == "best":
-            data_tmp = data_tmp.loc[(slice(None), slice(None), [
-                "1", "2", "3", "4", "5"]), :]
-        elif model == "first":
-            data_tmp = data_tmp.loc[(slice(None), slice(None),
-                                    "1"), :]
-        grouped = data_tmp.groupby(["group", "target"])
-        grouped = pd.DataFrame(grouped[feature].max())
-        grouped.index = grouped.index.droplevel(1)
-        # if there is any value in this column that is a string, convert it to float
-        grouped[feature] = grouped[feature].astype(float)
-        try:
-            # # find if there is any string in the dataframe
-            # for i in range(grouped.shape[0]):
-            #     for j in range(grouped.shape[1]):
-            #         if isinstance(grouped.iloc[i, j], str):
-            #             print(grouped.iloc[i, j])
-            grouped = grouped.sort_values(by=feature, ascending=False)
-            # print all the value in grouped
-            # for i in range(grouped.shape[0]):print(grouped.iloc[i])
-            initial_z = (grouped - grouped.mean()) / grouped.std()
-        except:
-            breakpoint()
-        new_z_score = pd.DataFrame(
-            index=grouped.index, columns=grouped.columns)
-        filtered_data = grouped[feature][initial_z[feature] >= -2]
-        new_mean = filtered_data.mean(skipna=True)
-        new_std = filtered_data.std(skipna=True)
-        new_z_score[feature] = (grouped[feature] - new_mean) / new_std
-        new_z_score = new_z_score.fillna(-2.0)
-        new_z_score = new_z_score.where(new_z_score > -2, -2)
-
-        # breakpoint()
-
-        # I actually don't understand why this is necessary... but need to keep it in mind.
-        # grouped = grouped.apply(lambda x: (x - x.mean()) / x.std())
-
-        new_z_score = new_z_score.rename(
-            columns={feature: csv_file.split(".")[0]})
-        data = pd.concat([data, new_z_score], axis=1)
-    # impute data again with -2
-    data = data.fillna(-2.0)
-
-    data.to_csv("./group_by_target/" +
-                "group_by_target-{}-{}-{}.csv".format(feature, model, mode))
-
-    data["sum"] = data.sum(axis=1)
-    data = data.sort_values(by="sum", ascending=False)
-    data.to_csv("./sum/" + "sum_{}-{}-{}.csv".format(feature, model, mode))
-
 
 for feature in features:
-    get_group_by_target(csv_list, csv_path, feature, model, mode)
+    get_group_by_target(csv_list, csv_path, sum_path,
+                        feature, model, mode, impute_value=impute_value)
     print("Finished processing {}".format(feature))
