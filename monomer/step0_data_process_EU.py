4e267e98e7ca4d93fe14bd2a0efcb1b4e8ab1b88
Rongqing Yuan <yuanrongqingyrq@gmail.com>
2024-11-20 01:08:05 -0500

    data process: 1. update input dir 2. compatible with Qian's format

--- monomer/step0_data_process_EU.py
+++ monomer/step0_data_process_EU.py
@@ -7,33 +7,43 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--stage', type=str, default="1")
+parser.add_argument('--input_path', type=str,
+                    # default="/home2/s439906/data/CASP16/monomers_EU_merge_v/"
+                    default="/home2/s439906/data/CASP16/monomer_inputs/"
+                    )
+parser.add_argument('--output_path', type=str,
+                    default="./monomer_data_Sep_15_EU/")
+
 args = parser.parse_args()
 stage = args.stage
+input_path = args.input_path
+output_path = args.output_path
 
-
-# monomer_path = "/home2/s439906/data/CASP16/monomers/"
 # monomer_path = "/home2/s439906/data/CASP16/monomers_Sep_8/"
 # monomer_path = "/home2/s439906/data/CASP16/monomers_Sep_10/"
 
 
-monomer_path = "/home2/s439906/data/CASP16/monomers_EU_merge_v/"
+monomer_path = input_path
 monomer_list = [txt for txt in os.listdir(
     monomer_path) if txt.endswith(".txt")]
-monomer_data_EU_path = "./monomer_data_Sep_15_EU/processed/"
-monomer_data_raw_EU_path = "./monomer_data_Sep_15_EU/raw_data/"
+monomer_data_EU_path = output_path + \
+    "processed/" if output_path[-1] == "/" else output_path + "/processed/"
+monomer_data_raw_EU_path = output_path + \
+    "raw_data/" if output_path[-1] == "/" else output_path + "/raw_data/"
 
 if stage == "1":
-    ...
+    monomer_list = [txt for txt in os.listdir(
+        monomer_path) if txt.endswith(".txt") and txt.startswith("T1")]
 elif stage == "0":
     monomer_path = "/home2/s439906/data/CASP16/monomers_Oct_3_T0/"
     monomer_list = [txt for txt in os.listdir(
-        monomer_path) if txt.endswith(".txt") and "D" in txt]
+        monomer_path) if txt.endswith(".txt")]
     monomer_data_EU_path = "./monomer_data_T0_EU/processed/"
     monomer_data_raw_EU_path = "./monomer_data_T0_EU/raw_data/"
 elif stage == "2":
     monomer_path = "/home2/s439906/data/CASP16/monomers_Oct_3_T2/"
     monomer_list = [txt for txt in os.listdir(
-        monomer_path) if txt.endswith(".txt") and "D" in txt]
+        monomer_path) if txt.endswith(".txt")]
     monomer_data_EU_path = "./monomer_data_T2_EU/processed/"
     monomer_data_raw_EU_path = "./monomer_data_T2_EU/raw_data/"
 
@@ -66,6 +76,10 @@ for monomer in monomer_list:
     except KeyError:
         print("KeyError: ", monomer)
         sys.exit()
+    # BUG 1.1 some monomer files have "MolProb" instead of "MolPrb_Score"
+    # set the "MolPrb_Score" column as the index
+    if "MolProb" in data.columns:
+        data = data.rename(columns={"MolProb": "MolPrb_Score"})
 
     # BUG 2 another strange bug. Some index line does not have -D* as the suffix. We need to fill them out
     if "-D" in monomer:
@@ -79,7 +93,10 @@ for monomer in monomer_list:
 
     data.to_csv(monomer_data_raw_EU_path + monomer[:-4] + ".csv")
     data = data.drop(["GR#", "#"], axis=1)
-    data = data.drop(["DipDiff", "BBscore", "SCscore"], axis=1)
+    try:
+        data = data.drop(["DipDiff", "BBscore", "SCscore"], axis=1)
+    except KeyError:
+        continue  # CASP15 does not have these columns
     data = data.drop(["RANK"], axis=1)
 
     data.replace("N/A", np.nan, inplace=True)
@@ -101,4 +118,4 @@ for monomer in monomer_list:
     new_z_score.to_csv(monomer_data_EU_path + monomer[:-4] + ".csv")
 
 time_2 = time.time()
-print("Time used: ", time_2 - time_1)
+# print("Time used: ", time_2 - time_1)
