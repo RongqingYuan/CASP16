57859be9a4d25ecd4493c683091c52fab2c3ee30
Rongqing Yuan <yuanrongqingyrq@gmail.com>
2024-11-20 01:09:02 -0500

    get score matrix: simplify code

--- monomer/step10_sum_z-score.py
+++ monomer/step10_sum_z-score.py
@@ -1,19 +1,19 @@
 import argparse
 import pandas as pd
-import numpy as np
 import os
 
 
-def get_group_by_target(csv_list, csv_path, out_path, sum_path,
+def get_group_by_target(csv_list, csv_path, out_path,
                         feature, model, mode, impute_value=-2):
     inverse_columns = ["RMS_CA", "RMS_ALL", "err",
                        "RMSD[L]", "MolPrb_Score", "FlexE", "MP_clash", "MP_rotout", "MP_ramout"]
-    if not os.path.exists(out_path + f"impute={impute_value}/"):
-        os.makedirs(out_path + f"impute={impute_value}/")
-    if not os.path.exists(out_path + "raw/"):
-        os.makedirs(out_path + "raw/")
-    if not os.path.exists(sum_path + f"impute={impute_value}/"):
-        os.makedirs(sum_path + f"impute={impute_value}/")
+
+    # if not os.path.exists(out_path + f"impute={impute_value}/"):
+    #     os.makedirs(out_path + f"impute={impute_value}/")
+    # if not os.path.exists(out_path + "raw/"):
+    #     os.makedirs(out_path + "raw/")
+    # if not os.path.exists(sum_path + f"impute={impute_value}/"):
+    #     os.makedirs(sum_path + f"impute={impute_value}/")
 
     data = pd.DataFrame()
     data_raw = pd.DataFrame()
@@ -23,21 +23,25 @@ def get_group_by_target(csv_list, csv_path, out_path, sum_path,
         data_tmp = pd.DataFrame(data_tmp[feature])
         # if there is "-" in the value, replace it with 0
         data_tmp = data_tmp.replace("-", float(0))
+
         if feature in inverse_columns:
             data_tmp[feature] = -data_tmp[feature]
+
         data_tmp.index = data_tmp.index.str.extract(
             r'(T\w+)TS(\w+)_(\w+)-(D\w+)').apply(lambda x: (f"{x[0]}-{x[3]}", f"TS{x[1]}", x[2]), axis=1)
         data_tmp.index = pd.MultiIndex.from_tuples(
             data_tmp.index, names=['target', 'group', 'submission_id'])
-        # # get all data with submission_id == 6
-        # data_tmp = data_tmp.loc[(slice(None), slice(None), "6"), :]
-        # drop all data with submission_id == 6
         if model == "best":
             data_tmp = data_tmp.loc[(slice(None), slice(None), [
                 "1", "2", "3", "4", "5"]), :]
         elif model == "first":
             data_tmp = data_tmp.loc[(slice(None), slice(None),
                                     "1"), :]
+        elif model == "sixth":
+            data_tmp = data_tmp.loc[(slice(None), slice(None),
+                                    "6"), :]
+        # if "all" in csv_file:
+        #     breakpoint()
         grouped = data_tmp.groupby(["group"])
         grouped = pd.DataFrame(grouped[feature].max())
         # grouped.index = grouped.index.droplevel(1)
@@ -59,7 +63,6 @@ def get_group_by_target(csv_list, csv_path, out_path, sum_path,
 
         grouped = grouped.rename(columns={feature: csv_file.split(".")[0]})
         data_raw = pd.concat([data_raw, grouped], axis=1)
-    # breakpoint()
 
     # impute data again with impute_value, because some group may not submit all targets
     data = data.fillna(impute_value)
@@ -68,8 +71,8 @@ def get_group_by_target(csv_list, csv_path, out_path, sum_path,
 
     data = data.reindex(sorted(data.columns), axis=1)
     data = data.sort_index()
-    data.to_csv(
-        out_path + f"impute={impute_value}/" + f"group_by_target-{feature}-{model}-{mode}.csv")
+    data_csv = f"{feature}-{model}-{mode}-impute={impute_value}.csv"
+    data.to_csv(out_path + data_csv)
 
     data_columns = data.columns
     target_count = {}
@@ -86,22 +89,44 @@ def get_group_by_target(csv_list, csv_path, out_path, sum_path,
 
     data["sum"] = data.sum(axis=1)
     data = data.sort_values(by="sum", ascending=False)
-    data.to_csv(
-        sum_path + f"impute={impute_value}/" + f"sum_unweighted_{feature}-{model}-{mode}.csv")
+    data_sum_unweighted_csv = f"{feature}-{model}-{mode}-impute={impute_value}-sum_unweighted.csv"
+    data.to_csv(out_path + data_sum_unweighted_csv)
 
     data.drop(columns=["sum"], inplace=True)
     data = data * pd.Series(EU_weight)
     data["sum"] = data.sum(axis=1)
     data = data.sort_values(by="sum", ascending=False)
-    data.to_csv(
-        sum_path + f"impute={impute_value}/" + f"sum_{feature}-{model}-{mode}.csv")
+    data_sum_csv = f"{feature}-{model}-{mode}-impute={impute_value}-sum.csv"
+    data.to_csv(out_path + data_sum_csv)
 
     data_raw = data_raw.reindex(sorted(data_raw.columns), axis=1)
     data_raw = data_raw.sort_index()
-    data_raw_csv = f"groups_by_targets_for-raw-{feature}-{model}-{mode}.csv"
-    data_raw.to_csv
-    data_raw.to_csv(
-        out_path + "raw/" + data_raw_csv)
+    data_raw_csv = f"{feature}-{model}-{mode}-raw.csv"
+    data_raw.to_csv(out_path + data_raw_csv)
+
+
+parser = argparse.ArgumentParser(description="options for sum z-score")
+
+parser.add_argument('--csv_path', type=str,
+                    default="./monomer_data_Sep_15_EU/raw_data/")
+parser.add_argument('--out_path', type=str, default="./group_by_target_EU/")
+parser.add_argument('--model', type=str,
+                    help='first, best or sixth', default='best')
+parser.add_argument('--mode', type=str,
+                    help='easy, medium, hard or all', default='all')
+parser.add_argument('--impute_value', type=int, default=-2)
+
+args = parser.parse_args()
+csv_path = args.csv_path
+out_path = args.out_path
+model = args.model
+mode = args.mode
+impute_value = args.impute_value
+
+
+csv_list = [txt for txt in os.listdir(
+    csv_path) if txt.endswith(".csv") and txt.startswith("T")]
+csv_list = sorted(csv_list)
 
 
 hard_group = [
@@ -186,44 +211,6 @@ easy_group = [
     "T1295-D2",
     "T1299-D1",
 ]
-print(len(easy_group))
-print(len(medium_group))
-print(len(hard_group))
-
-
-parser = argparse.ArgumentParser(description="options for sum z-score")
-
-parser.add_argument('--csv_path', type=str,
-                    default="./monomer_data_Sep_15_EU/raw_data/")
-parser.add_argument('--sum_path', type=str, default="./sum_EU/")
-parser.add_argument('--out_path', type=str, default="./group_by_target_EU/")
-parser.add_argument('--model', type=str, help='first or best', default='best')
-parser.add_argument('--mode', type=str,
-                    help='easy, medium, hard or all', default='all')
-parser.add_argument('--impute_value', type=int, default=-2)
-parser.add_argument('--stage', type=str, default="1")
-
-args = parser.parse_args()
-csv_path = args.csv_path
-sum_path = args.sum_path
-out_path = args.out_path
-model = args.model
-mode = args.mode
-impute_value = args.impute_value
-# stage = args.stage
-# if stage == "1":
-#     csv_list = [txt for txt in os.listdir(
-#         csv_path) if txt.endswith(".csv") and txt.startswith("T1")]
-# elif stage == "0":
-#     csv_list = [txt for txt in os.listdir(
-#         csv_path) if txt.endswith(".csv") and txt.startswith("T0")]
-# elif stage == "2":
-#     csv_list = [txt for txt in os.listdir(
-#         csv_path) if txt.endswith(".csv") and txt.startswith("T2")]
-csv_list = [txt for txt in os.listdir(
-    csv_path) if txt.endswith(".csv") and txt.startswith("T")]
-csv_list = sorted(csv_list)
-
 
 if mode == "hard":
     csv_list = [csv for csv in csv_list if csv.split(
@@ -245,15 +232,17 @@ for csv in csv_list:
 
 if not os.path.exists(out_path):
     os.makedirs(out_path)
-if not os.path.exists(sum_path):
-    os.makedirs(sum_path)
 features = ['GDT_TS',
             'GDT_HA', 'GDC_SC', 'GDC_ALL', 'RMS_CA', 'RMS_ALL', 'AL0_P',
             'AL4_P', 'ALI_P', 'LGA_S', 'RMSD[L]', 'MolPrb_Score', 'LDDT',
             'SphGr',
             'CAD_AA', 'RPF', 'TMscore', 'FlexE', 'QSE', 'CAD_SS', 'MP_clash',
-            'MP_rotout', 'MP_ramout', 'MP_ramfv', 'reLLG_lddt', 'reLLG_const']
+            # 'MP_rotout',
+            # 'MP_ramout',
+            # 'MP_ramfv',
+            'reLLG_lddt',
+            'reLLG_const']
 for feature in features:
-    get_group_by_target(csv_list, csv_path, out_path, sum_path,
+    get_group_by_target(csv_list, csv_path, out_path,
                         feature, model, mode, impute_value=impute_value)
     print("Finished processing {}".format(feature))
