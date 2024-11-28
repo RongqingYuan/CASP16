6dbdb6f52bcb30a3347f87ed6293c65ecf8bfcdf
Rongqing Yuan <yuanrongqingyrq@gmail.com>
2024-11-28 16:27:38 -0500

    Merge branch 'dev' into main

---
+++ monomer/z_score.sh
@@ -0,0 +1,17 @@
+python step10a_sum_z-score.py 
+python step10a_sum_z-score.py --phase 0
+python step10a_sum_z-score.py --phase 1
+python step10a_sum_z-score.py --phase 2
+python step10a_sum_z-score.py --model first
+python step10a_sum_z-score.py --mode easy
+python step10a_sum_z-score.py --mode medium
+python step10a_sum_z-score.py --mode difficult
+
+# python sum_z-score.py 
+# python sum_z-score.py --phase 0
+# python sum_z-score.py --phase 1
+# python sum_z-score.py --phase 2
+# python sum_z-score.py --model first
+# python sum_z-score.py --mode easy
+# python sum_z-score.py --mode medium
+# python sum_z-score.py --mode difficult
