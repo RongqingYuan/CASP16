ba93fe25b265e0ff992ef4d4b8720c1fbbda1441
Rongqing Yuan <yuanrongqingyrq@gmail.com>
2024-11-28 16:32:21 -0500

    good version for monomer score

--- monomer/step0_data_process_EU.py
+++ monomer/step0_data_process_EU.py
@@ -1,12 +1,10 @@
 import os
-import numpy as np
 import pandas as pd
 import sys
-import time
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--stage', type=str, default="all")
+parser.add_argument('--phase', type=str, default="0,1,2")
 parser.add_argument('--input_path', type=str,
                     # default="/home2/s439906/data/CASP16/monomers_EU_merge_v/"
                     # default="/home2/s439906/data/CASP16/monomer_inputs/"
@@ -16,7 +14,7 @@ parser.add_argument('--output_path', type=str,
                     default="./monomer_data_newest/")
 
 args = parser.parse_args()
-stage = args.stage
+phase = args.phase
 input_path = args.input_path
 output_path = args.output_path
 
@@ -29,27 +27,20 @@ monomer_data_EU_path = output_path + \
 monomer_data_raw_EU_path = output_path + \
     "raw_data/" if output_path[-1] == "/" else output_path + "/raw_data/"
 
-if stage == "1":
+if phase == "1":
     monomer_list = [txt for txt in os.listdir(
         monomer_path) if txt.endswith(".txt") and txt.startswith("T1")]
-elif stage == "0":
-    # monomer_path = "/home2/s439906/data/CASP16/monomers_Oct_3_T0/"
-    # monomer_list = [txt for txt in os.listdir(
-    #     monomer_path) if txt.endswith(".txt")]
+elif phase == "0":
     monomer_list = [txt for txt in os.listdir(
         monomer_path) if txt.endswith(".txt") and txt.startswith("T0")]
     monomer_data_EU_path = "./monomer_data_T0_EU/processed/"
     monomer_data_raw_EU_path = "./monomer_data_T0_EU/raw_data/"
-elif stage == "2":
-    # monomer_path = "/home2/s439906/data/CASP16/monomers_Oct_3_T2/"
-    # monomer_list = [txt for txt in os.listdir(
-    #     monomer_path) if txt.endswith(".txt")]
+elif phase == "2":
     monomer_list = [txt for txt in os.listdir(
         monomer_path) if txt.endswith(".txt") and txt.startswith("T2")]
     monomer_data_EU_path = "./monomer_data_T2_EU/processed/"
     monomer_data_raw_EU_path = "./monomer_data_T2_EU/raw_data/"
-
-elif stage == "all":
+elif phase == "0,1,2":
     monomer_list = [txt for txt in os.listdir(
         monomer_path) if txt.endswith(".txt")]
     monomer_data_EU_path = output_path + \
@@ -63,7 +54,6 @@ if not os.path.exists(monomer_data_raw_EU_path):
 monomer_list.sort()
 
 
-time_1 = time.time()
 for monomer in monomer_list:
     monomer_file = monomer_path + monomer
     data = []
@@ -85,12 +75,10 @@ for monomer in monomer_list:
     except KeyError:
         print("KeyError: ", monomer)
         sys.exit()
-    # BUG 1.1 some monomer files have "MolProb" instead of "MolPrb_Score"
-    # set the "MolPrb_Score" column as the index
+    # BUG 1.1 some monomer files have "MolProb" instead of "MolPrb_Score". Set the "MolPrb_Score" column as the index
     if "MolProb" in data.columns:
         data = data.rename(columns={"MolProb": "MolPrb_Score"})
 
-    # get a deep copy of the data
     data_copy = data.copy()
     # BUG 2 another strange bug. Some index line does not have -D* as the suffix. We need to fill them out
     if "-D" in monomer:
@@ -104,40 +92,6 @@ for monomer in monomer_list:
 
     data_copy.to_csv(monomer_data_raw_EU_path + monomer[:-4] + ".csv")
 
-    # data = data.drop(["GR#", "#"], axis=1)
-    # try:
-    #     data = data.drop(["DipDiff", "BBscore", "SCscore"], axis=1)
-    # except KeyError:
-    #     continue  # CASP15 does not have these columns
-    # data = data.drop(["RANK"], axis=1)
-
-    # data.replace("N/A", np.nan, inplace=True)
-    # data.replace("-", np.nan, inplace=True)
-    # data = data.astype(float)
-    # inverse_columns = ["RMS_CA", "RMS_ALL", "err",
-    #                    "RMSD[L]", "MolPrb_Score", "FlexE", "MP_clash", "MP_rotout", "MP_ramout"]
-    # data[inverse_columns] = -data[inverse_columns]
-    # initial_z = (data - data.mean()) / data.std()
-
-    # new_z_score = pd.DataFrame(index=data.index, columns=data.columns)
-    # for column in data.columns:
-    #     filtered_data = data[column][initial_z[column] >= -2]
-    #     new_mean = filtered_data.mean(skipna=True)
-    #     new_std = filtered_data.std(skipna=True)
-    #     new_z_score[column] = (data[column] - new_mean) / new_std
-    # new_z_score = new_z_score.fillna(-2.0)
-    # new_z_score = new_z_score.where(new_z_score > -2, -2)
-    # # save the following columns:
-    # to_save = [
-    #     'GDT_HA', 'GDC_SC', 'reLLG_const',
-    #     'QSE',
-    #     'AL0_P', 'SphGr',
-    #     'CAD_AA', 'LDDT',
-    #     'MolPrb_Score',
-    # ]
-    # new_z_score = new_z_score[to_save]
-    # new_z_score.to_csv(monomer_data_EU_path + monomer[:-4] + ".csv")
-
     to_save = [
         'GDT_HA', 'GDC_SC', 'reLLG_const',
         'QSE',
@@ -146,27 +100,23 @@ for monomer in monomer_list:
         'MolPrb_Score',
     ]
     data = data[to_save]
-    # breakpoint()
     data = data[~data.index.str.contains('_6')]
-    # data = data.replace("N/A", np.nan)
-    # data = data.replace("-", np.nan)
     max_MP = data['MolPrb_Score'].replace(
         ['N/A', '-'], pd.NA).dropna().astype(float).max()
-    # max_value = 10
     data['MolPrb_Score'] = data['MolPrb_Score'].replace(
         ['N/A', '-'], max_MP)
     min_QSE = data['QSE'].replace(
         ['N/A', '-'], pd.NA).dropna().astype(float).min()
     data['QSE'] = data['QSE'].replace(
         ['N/A', '-'], min_QSE)
-    # min_reLLG = data['reLLG_const'].replace(
-    #     ['N/A', '-'], pd.NA).dropna().astype(float).min()
-    # data['reLLG_const'] = data['reLLG_const'].replace(
-    #     ['N/A', '-'], min_reLLG)
-    # min_AL0_P = data['AL0_P'].replace(
-    #     ['N/A', '-'], pd.NA).dropna().astype(float).min()
-    # data['AL0_P'] = data['AL0_P'].replace(
-    #     ['N/A', '-'], min_AL0_P)
+    min_reLLG = data['reLLG_const'].replace(
+        ['N/A', '-'], pd.NA).dropna().astype(float).min()
+    data['reLLG_const'] = data['reLLG_const'].replace(
+        ['N/A', '-'], min_reLLG)
+    min_AL0_P = data['AL0_P'].replace(
+        ['N/A', '-'], pd.NA).dropna().astype(float).min()
+    data['AL0_P'] = data['AL0_P'].replace(
+        ['N/A', '-'], min_AL0_P)
     min_SphGr = data['SphGr'].replace(
         ['N/A', '-'], pd.NA).dropna().astype(float).min()
     data['SphGr'] = data['SphGr'].replace(
@@ -179,8 +129,6 @@ for monomer in monomer_list:
         ['N/A', '-'], pd.NA).dropna().astype(float).min()
     data['LDDT'] = data['LDDT'].replace(
         ['N/A', '-'], min_LDDT)
-    # data = data.replace("N/A", 0)
-    # data = data.replace("-", 0)
     data = data.astype(float)
     inverse_columns = ["MolPrb_Score"]
     data[inverse_columns] = -data[inverse_columns]
@@ -221,5 +169,3 @@ for monomer in monomer_list:
     with open(monomer_data_EU_path + monomer[:-4] + ".csv", "w") as f:
         for line in lines:
             f.write(line)
-
-time_2 = time.time()
