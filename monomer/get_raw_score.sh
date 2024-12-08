caf5d155ae1a3c700e5ca7ebf3702db26ecf1da7
Rongqing Yuan <yuanrongqingyrq@gmail.com>
2024-12-08 18:07:36 -0500

    rename raw score and z score code

---
+++ monomer/get_raw_score.sh
@@ -0,0 +1,4 @@
+python get_raw_score.py --phase 0 --out_path ./score_T0/
+python get_raw_score.py --phase 1 --out_path ./score_T1/
+python get_raw_score.py --phase 2 --out_path ./score_T2/
+python get_raw_score.py
